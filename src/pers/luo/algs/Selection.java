7fc0dff651557a965910274c2682c7e50834b0f1
Luo Wenyang <luowyang@gmail.com>
2020-01-22 23:35:24 +0800

    Update Selection.java

--- src/pers/luo/algs/Selection.java
+++ src/pers/luo/algs/Selection.java
@@ -6,7 +6,13 @@ import edu.princeton.cs.algs4.StdIn;
 public class Selection {
     public static void sort(Comparable[] a)
     {
-
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            int min = i;
+            for (int j = i + 1; j < N; j++)
+                if (less(a[j], a[min])) min = j;
+            exch(a, i, min);
+        }
     }
 
     public static boolean less(Comparable v, Comparable w)
