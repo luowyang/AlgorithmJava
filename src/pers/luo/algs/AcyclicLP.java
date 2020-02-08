ed9d6ebdaf47a0e51cf08dc0f4333083288e1883
Luo Wenyang <luowyang@gmail.com>
2020-02-08 20:09:49 +0800

    create AcyclicLP

--- src/pers/luo/algs/AcyclicLP.java
+++ src/pers/luo/algs/AcyclicLP.java
@@ -1,47 +1,44 @@
 package pers.luo.algs;
 
 /*
- * Single-Source Shortest Path Algorithm for acyclic directed graph
+ * Single-Source Longest Path Algorithm for acyclic directed graph
  */
-public class AcyclicSP implements SP {
+public class AcyclicLP implements SP {
     private DirectedEdge[] edgeTo;  // last edge of shortest path
     private double[] distTo;        // distance to source
 
-    public AcyclicSP(EdgeWeightedDigraph G, int s) {
+    public AcyclicLP(EdgeWeightedDigraph G, int s) {
         edgeTo = new DirectedEdge[G.V()];           // edgeTo[s] should be null, which is a default value
         distTo = new double[G.V()];
         for (int v = 0; v < G.V(); v++)
-            distTo[v] = Double.POSITIVE_INFINITY;   // initially all vertices beyond s is unreachable
+            distTo[v] = Double.NEGATIVE_INFINITY;   // initially all vertices beyond s is unreachable and negatively infinite
         distTo[s] = 0.0;                            // source has 0.0 distance
 
         Topological top = new Topological(G);
-        for (int v : top.order())                   // no need to handle vertices before s as their distances are infinity
-            relax(G, v);                            // relax vertices in topological order
+        for (int v : top.order())                   // no need to handle vertices before s as their distances are negatively infinity
+            tight(G, v);                            // relax vertices in topological order
     }
 
-    // vertex relaxation: relax all edges from v
-    private void relax(EdgeWeightedDigraph G, int v) {
-        for (DirectedEdge e : G.adj(v)) {               // all edges from v will be relaxed
+    // vertex tightening: tight all edges from v
+    private void tight(EdgeWeightedDigraph G, int v) {
+        for (DirectedEdge e : G.adj(v)) {               // all edges from v will be tightened
             int w = e.to();
-            if (distTo[w] > distTo[v] + e.weight()) {   // include the case where w is unreachable
-                // if s-->v->w is shorter than the known shortest path s-->w, update SPT
+            if (distTo[w] < distTo[v] + e.weight()) {   // include the case where w is unreachable
+                // if s-->v->w is longer than the known longest path s-->w, update LPT
                 distTo[w] = distTo[v] + e.weight();
                 edgeTo[w] = e;
             }
         }
     }
 
-    @Override
     public double distTo(int v) {
         return distTo[v];
     }
 
-    @Override
     public boolean hasPathTo(int v) {
-        return distTo[v] < Double.POSITIVE_INFINITY;
+        return distTo[v] > Double.NEGATIVE_INFINITY;
     }
 
-    @Override
     public Iterable<DirectedEdge> pathTo(int v) {
         Stack<DirectedEdge> path = new Stack<>();
         for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()])
@@ -52,11 +49,11 @@ public class AcyclicSP implements SP {
     public static void main(String[] args) {
         EdgeWeightedDigraph G = new EdgeWeightedDigraph(System.in);
         int s = Integer.parseInt(args[0]);
-        AcyclicSP sp = new AcyclicSP(G, s);
+        AcyclicLP lp = new AcyclicLP(G, s);
         for (int v = 0; v < G.V(); v++) {
-            System.out.printf("%d to %d (%f): ", s, v, sp.distTo(v));
-            if (sp.hasPathTo(v))
-                for (DirectedEdge e : sp.pathTo(v))
+            System.out.printf("%d to %d (%f): ", s, v, lp.distTo(v));
+            if (lp.hasPathTo(v))
+                for (DirectedEdge e : lp.pathTo(v))
                     System.out.print(e + "  ");
             System.out.println();
         }
