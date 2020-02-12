4b8606a2e207966b2464f69bfc07399720577bdd
Luo Wenyang <luowyang@gmail.com>
2020-02-12 13:55:50 +0800

    Update TST.java

--- src/pers/luo/algs/TST.java
+++ src/pers/luo/algs/TST.java
@@ -27,14 +27,14 @@ public class TST<Value> implements StringST<Value> {
     public Value get(String key) {
         Node<Value> cur = root;
         int d = 0, n = key.length();
-        char c = key.charAt(d);
-        while (cur != null) {
+        while (cur != null && d < n) {
+            char c = key.charAt(d);
             if      (c < cur.c) cur = cur.left;
             else if (c > cur.c) cur = cur.right;
-            else if (++d < n)   { cur = cur.mid; c = key.charAt(d); }
-            else                return cur.value;
+            else if (++d < n)   cur = cur.mid;
         }
-        return null;
+        if (cur == null) return null;
+        return cur.value;
     }
 
     @Override
@@ -66,7 +66,7 @@ public class TST<Value> implements StringST<Value> {
         }
         // update size
         node.size = (node.value == null) ? 0 : 1;
-        node.size += size(node.left) + size(node.right);
+        node.size += size(node.left) + size(node.mid) + size(node.right);
         return node;
     }
 
@@ -101,17 +101,83 @@ public class TST<Value> implements StringST<Value> {
 
     @Override
     public Iterable<String> keysWithPrefix(String pre) {
-        return null;
+        // find the prefix's corresponding node
+        Node<Value> cur = root;
+        int d = 0, n = pre.length();
+        Queue<String> queue = new Queue<>();
+        while (cur != null && d < n) {
+            char c = pre.charAt(d);
+            if      (c < cur.c) cur = cur.left;
+            else if (c > cur.c) cur = cur.left;
+            else {
+                if (++d == n && cur.value != null)
+                    queue.enqueue(pre);
+                cur = cur.mid;
+            }
+        }
+        // cur is either null or prefix's tail char's corresponding node
+        if (cur != null) collect(cur, pre, queue);
+        return queue;
+    }
+
+    // collect all keys within node's subtree
+    // note that pre is the common prefix reaching node from a middle link
+    private void collect(Node<Value> node, String pre, Queue<String> queue) {
+        if (node == null) return;
+        collect(node.left, pre, queue);
+        if (node.value != null) queue.enqueue(pre + node.c);
+        collect(node.mid, pre + node.c, queue);     // note that the prefix of the middle subtree is one char longer
+        collect(node.right, pre, queue);
     }
 
     @Override
     public Iterable<String> keysThatMatch(String pat) {
-        return null;
+        Queue<String> queue = new Queue<>();
+        collect(root, "", pat, queue);
+        return queue;
+    }
+
+    // collect all keys within node's subtree which match the pattern string pat
+    // note that pre is the common prefix reaching node from a middle link
+    private void collect(Node<Value> node, String pre, String pat, Queue<String> queue) {
+        if (node == null) return;
+        int d = pre.length();   // next char of pat to match
+        char c = pat.charAt(d);
+        if (c == '.') { // wildcard
+            collect(node.left, pre, pat, queue);
+            if (++d == pat.length()) {
+                if (node.value != null)
+                    queue.enqueue(pre + node.c);
+            }
+            else {
+                collect(node.mid, pre + node.c, pat, queue);
+            }
+            collect(node.right, pre, pat, queue);
+        }
+        else if (c < node.c) collect(node.left, pre, pat, queue);
+        else if (c > node.c) collect(node.right, pre, pat, queue);
+        else if (++d == pat.length()) {
+            if (node.value != null) queue.enqueue(pre + node.c);
+        }
+        else                 collect(node.mid, pre + node.c, pat, queue);
     }
 
     @Override
     public String longestPrefixOf(String s) {
-        return null;
+        Node<Value> cur = root;
+        int d = 0, n = s.length();
+        int length = 0;
+        while (cur != null && d < n) {
+            char c = s.charAt(d);
+            if      (c < cur.c) cur = cur.left;
+            else if (c > cur.c) cur = cur.right;
+            else {
+                d++;
+                if (cur.value != null) length = d;
+                cur = cur.mid;
+            }
+        }
+        return s.substring(0, length);
     }
 
     public static void main(String[] args) throws FileNotFoundException {
