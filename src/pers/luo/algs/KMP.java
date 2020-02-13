3505b847eb78fa2d323b857e507a0e08303a3650
Luo Wenyang <luowyang@gmail.com>
2020-02-13 19:43:25 +0800

    Create KMP.java

---
+++ src/pers/luo/algs/KMP.java
@@ -0,0 +1,87 @@
+package pers.luo.algs;
+
+import java.util.Scanner;
+
+/**
+ * Knuth-Morris-Pratt substring searching algorithm
+ * @author Luo Wenyang
+ */
+public class KMP {
+    private static final int R = 256;
+    private String pat;     // pattern string
+    private int[][] dfa;    // state-table of DFA
+
+    public KMP(String pat) {
+        this.pat = pat;
+        // build DFA
+        buildDFA();
+    }
+
+    private void buildDFA() {
+        int X, j, M = pat.length();
+        dfa = new int[R][M];
+        // set dfa[][0]
+        dfa[pat.charAt(0)][0] = 1;
+        // set other dfa[][], do not start with j=0 or X will be set to wrong value for j=1
+        for (X = 0, j = 1; j < M; j++) {
+            // calculate dfa when mismatched
+            for (char c = 0; c < R; c++)
+                dfa[c][j] = dfa[c][X];
+            // calculate dfa when matched
+            dfa[pat.charAt(j)][j] = j + 1;
+            // update X for j + 1
+            X = dfa[pat.charAt(j)][X];
+        }
+    }
+
+    public void setPattern(String pat) {
+        this.pat = pat;
+        buildDFA();
+    }
+
+    public int search(String txt) {
+        int N = txt.length();
+        int M = pat.length();
+        int i, j;   // i is the text pointer, j is the pattern pointer and the current state
+        for (i = 0, j = 0; i < N && j < M; i++)
+            j = dfa[txt.charAt(i)][j];
+        return j == M ? i - M : N;
+    }
+
+    public String toString() {
+        int M = pat.length();
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < M; j++)
+                s.append(dfa[i][j]).append(" ");
+            s.append("\n");
+        }
+        return s.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String txt = scanner.nextLine();
+            String pat = scanner.nextLine();
+            KMP kmp = new KMP(pat);
+            print(txt, pat, kmp.search(txt));
+        }
+    }
+
+    // print text and pattern in a distinguished format
+    private static void print(String txt, String pat, int i) {
+        System.out.println(i == txt.length() ? "No match" : "match");
+        System.out.print("text   : ");
+        for (char c : txt.toCharArray())
+            System.out.print(c + " ");
+        System.out.println();
+        System.out.print("pattern: ");
+        i %= txt.length();
+        for (int j = 0; j < i; j++)
+            System.out.print("  ");
+        for (char c : pat.toCharArray())
+            System.out.print(c + " ");
+        System.out.println();
+    }
+}
