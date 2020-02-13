7419cf06318c698a7bcf27c9f75362abdcd22d67
Luo Wenyang <luowyang@gmail.com>
2020-02-13 23:47:18 +0800

    Update Brute.java

--- src/pers/luo/algs/Brute.java
+++ src/pers/luo/algs/Brute.java
@@ -1,5 +1,7 @@
 package pers.luo.algs;
 
+import edu.princeton.cs.algs4.In;
+
 import java.util.Scanner;
 
 /**
@@ -13,15 +15,46 @@ public class Brute {
         this.pat = pat;
     }
 
+    public void pattern(String pat) {
+        this.pat = pat;
+    }
+
     public int search(String txt) {
+        return search(txt, 0);
+    }
+
+    public int search(String txt, int offset) {
         int M = pat.length();
         int N = txt.length();
-        for (int i = 0; i <= N - M; i++) {
+        for (int i = offset; i <= N - M; i++) {
             int j = 0;
             while (j < M && pat.charAt(j) == txt.charAt(i + j)) j++;
             if (j == M) return i;
         }
         return N;
+    }
+
+    public int count(String txt) {
+        int n = 0, i = 0, N = txt.length();
+        while (true) {
+            i = search(txt, i);
+            if (i == N) break;
+            i++;
+            n++;
+        }
+        return n;
+    }
+
+    public Iterable<Integer> searchAll(String txt) {
+        int i = 0, N = txt.length();
+        Queue<Integer> queue = new Queue<>();
+        while (true) {
+            i = search(txt, i);
+            if (i == N) break;
+            queue.enqueue(i);
+            i++;
+        }
+        return queue;
     }
 
     public static void main(String[] args) {
@@ -30,6 +63,9 @@ public class Brute {
         String pat = scanner.nextLine();
         Brute brute = new Brute(pat);
         print(txt, pat, brute.search(txt));
+        System.out.println("count: " + brute.count(txt));
+        for (int i : brute.searchAll(txt))
+            System.out.print(i + " ");
     }
 
     // print text and pattern in a distinguished format
