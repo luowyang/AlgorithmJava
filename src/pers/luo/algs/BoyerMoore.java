f191d6f3340a16653a678b9d5df54ec2d54cabec
Luo Wenyang <luowyang@gmail.com>
2020-02-14 13:35:15 +0800

    Update BoyerMoore.java

--- src/pers/luo/algs/BoyerMoore.java
+++ src/pers/luo/algs/BoyerMoore.java
@@ -1,5 +1,8 @@
 package pers.luo.algs;
 
+import edu.princeton.cs.algs4.In;
+
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -12,6 +15,28 @@ public class BoyerMoore {
     private int[] right;    // jump table
 
     private int R = 256;
+
+    private static class CharBuffer {
+        char[] buffer;
+        int size;       // size of buffer
+        int first;      // first char in buffer
+        int last;       // 1 char after the last char in buffer, position to add a new char
+        public CharBuffer(int size) {
+            this.size = size + 1;
+            buffer = new char[this.size];
+        }
+        public char read(int i) {
+            return buffer[(first + i) % size];
+        }
+        public void add(char c) {
+            buffer[last++] = c;
+            last %= size;
+        }
+        public void remove() {
+            buffer[first++] = 0;
+            first %= size;
+        }
+    }
 
     public BoyerMoore(String pat) {
         this.pat = pat;
@@ -41,15 +66,47 @@ public class BoyerMoore {
         int skip;   // skip distance of i
         for (int i = offset; i <= N - M; i += skip) {
             skip = 0;   // reset skip to 0
-            for (int j = M - 1; j >= 0; j--)
-                if (txt.charAt(i+j) != pat.charAt(j)) { // mismatched
-                    skip = j - right[txt.charAt(i+j)];  // calculate skip, including cases 1 and 2b
+            for (int j = M - 1; j >= 0; j--) {
+                if (txt.charAt(i + j) != pat.charAt(j)) { // mismatched
+                    skip = j - right[txt.charAt(i + j)];  // calculate skip, including cases 1 and 2b
                     if (skip < 1) skip = 1;     // case 2b
                     break;
                 }
+            }
             if (skip == 0) return i;    // match success
         }
         return N;
+    }
+
+    public Iterable<Integer> search(InputStream in) {
+        Scanner scanner = new Scanner(in);
+        scanner.useDelimiter("");
+        Queue<Integer> queue = new Queue<>();
+        int M = pat.length();
+        CharBuffer buffer = new CharBuffer(M);
+        int i = 0, skip = M;    // skip distance is also the number of chars to read into buffer
+        while (scanner.hasNext()) {
+            int k;
+            for (k = 0; k < skip && scanner.hasNext(); k++)
+                buffer.add(scanner.next().charAt(0));
+            if (k < skip) break;
+            skip = 0;
+            for (int j = M-1; j >= 0; j--) {
+                if (buffer.read(j) != pat.charAt(j)) {
+                    skip = j - right[buffer.read(j)];
+                    if (skip < 1) skip = 1;
+                    break;
+                }
+            }
+            if (skip == 0) {
+                queue.enqueue(i);
+                skip = 1;           // if matched, i need to skip 1 char
+            }
+            for (int j = 0; j < skip; j++)
+                buffer.remove();
+            i += skip;
+        }
+        return queue;
     }
 
     public int count(String txt) {
@@ -76,7 +133,11 @@ public class BoyerMoore {
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        BoyerMoore bm = new BoyerMoore("ABABAC");
+        for (int i : bm.search(System.in)) {
+            System.out.print(i + " ");
+        }
+        /*Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String txt = scanner.nextLine();
             String pat = scanner.nextLine();
@@ -85,7 +146,7 @@ public class BoyerMoore {
             System.out.println("count: " + bm.count(txt));
             for (int i : bm.searchAll(txt))
                 System.out.print(i + " ");
-        }
+        }*/
     }
 
     // print text and pattern in a distinguished format
