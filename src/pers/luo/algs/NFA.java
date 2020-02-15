aaffa468752eacc059be33b275906cc434fe2a35
Luo Wenyang <luowyang@gmail.com>
2020-02-15 14:58:59 +0800

    Update NFA.java

--- src/pers/luo/algs/NFA.java
+++ src/pers/luo/algs/NFA.java
@@ -1,12 +1,14 @@
 package pers.luo.algs;
 
-import edu.princeton.cs.algs4.In;
-
-import java.net.SecureCacheResponse;
 import java.util.Scanner;
 
 /**
  * Regular expression matching using Nondeterministic Finite Automaton
+ *
+ * Some rules for regular expressions:
+ * 1. No {,m}
+ * 2. No * or + after {}
+ *
  * @author Luo Wenyang
  */
 public class NFA {
@@ -40,38 +42,113 @@ public class NFA {
     }
 
     public NFA(String regex) {
-        re = regex.toCharArray();
-        M = regex.length();
+        Queue<Integer> metas = new Queue<>();   // used to deal with {n,m}, stores m-n
+        parseRE(regex, metas);
+        M = re.length;
         G = new Digraph(M + 1);
-        buildNFA();
+        compile(metas);
+    }
+
+    // parse RE to filter mistakes and convert regex to compilable form
+    private void parseRE(String regex, Queue<Integer> metas) {
+        StringBuilder reg = new StringBuilder();
+        Stack<Integer> symbols = new Stack<>();
+        int len = regex.length();
+        for (int i = 0; i < len; i++) {
+            char c = regex.charAt(i);
+            reg.append(c);
+            int rp = reg.length();      // 1 pos after right parenthesis or single char
+            int lp = rp - 1;            // pos of left parenthesis or single char
+            if (c == '(') symbols.push(lp);
+            else if (c == ')') lp = symbols.pop();
+            if (i < len - 3) {
+                char peek = regex.charAt(i + 1);
+                if (peek == '{') {          // deal with {} or {,}
+                    int beginFirst = (i += 2);
+                    int beginSecond;
+                    peek = regex.charAt(i);
+                    while (peek != ',' && peek != '}'){
+                        i++;
+                        peek = regex.charAt(i);
+                    }
+                    int n = Integer.parseInt(regex.substring(beginFirst, i));
+                    if (peek == '}') {      // {n}
+                        for (int j = 0; j < n - 1; j++)
+                            reg.append(reg.substring(lp, rp));
+                    }
+                    else {                  // {n,} or {n,m}
+                        beginSecond = ++i;
+                        peek = regex.charAt(i);
+                        if (peek == '}') {  // {n,}
+                            for (int j = 0; j < n - 1; j++)
+                                reg.append(reg.substring(lp, rp));
+                            reg.append('+');
+                        }
+                        else {              // {n,m}
+                            while (peek != '}'){
+                                i++;
+                                peek = regex.charAt(i);
+                            }
+                            int m = Integer.parseInt(regex.substring(beginSecond, i));
+                            for (int j = 0; j < m - 1; j++)
+                                reg.append(reg.substring(lp, rp));
+                            reg.append('{');
+                            metas.enqueue(m - n);               // number of forward links
+                        }
+                    }
+                }
+            }
+        }
+        re =  reg.toString().toCharArray();
     }
 
     // build NFA with stack
-    private void buildNFA() {
+    private void compile(Queue<Integer> metas) {
         Stack<Integer> ops = new Stack<>();
         for (int i = 0; i < M; i++) {
             int lp = i;     // left parenthesis or single char
             if (re[i] == '(' || re[i] == '|')
                 ops.push(i);
             else if(re[i] == ')') {
-                int or = ops.pop();     // top op, may be | or (
-                if (re[or] == '|') {    // if |
-                    lp = ops.pop();     // lp is now (
-                    G.addEdge(lp, or + 1);
-                    G.addEdge(or, i);
-                }
-                else {                  // if (
-                    lp = or;            // lp is now (
+                Bag<Integer> ors = new Bag<>();
+                int op;
+                for (op = ops.pop(); re[op] == '|'; op = ops.pop())
+                    ors.add(op);        // pop all |'s
+                lp = op;
+                for (int or : ors) {
+                    G.addEdge(lp, or + 1);  // add transition from lp to or+1
+                    G.addEdge(or, i);           // add transition from or to rp
                 }
             }
-            if (i < M-1 && re[i+1] == '*') {    // if next is *
-                // note that lp is ( or single char
-                G.addEdge(i+1, lp);
-                G.addEdge(lp, i+1);
+            if (i < M - 1) {
+                // note that lp is ( or single char, depending on whether re[i] is ) or not
+                if (re[i + 1] == '*') {
+                    // if next is *
+                    G.addEdge(i + 1, lp);
+                    G.addEdge(lp, i + 1);
+                }
+                else if (re[i + 1] == '+') {
+                    // if next is +
+                    G.addEdge(i + 1, lp);
+                }
+                else if (re[i + 1] == '{') {
+                    // if next is {
+                    int k = metas.dequeue();    // m-n
+                    int skip = i + 1 - lp;
+                    for (int j = 0; j < k; j++) {
+                        G.addEdge(lp, i + 1);
+                        lp = lp - skip;
+                    }
+                }
             }
-            if (re[i] == '(' || re[i] == '*' || re[i] == ')')
-                G.addEdge(i, i+1);          // add epsilon transition to next state for these ops
+            addEpsilonTransitionToNextState(i);
         }
+    }
+
+    private void addEpsilonTransitionToNextState(int i) {
+        if (re[i] == '(' || re[i] == ')' || re[i] == '*' || re[i] == '+'
+            || re[i] == '{')
+            G.addEdge(i, i+1);          // add epsilon transition to next state for these ops
     }
 
     // simulate NFA
@@ -103,7 +180,7 @@ public class NFA {
     }
 
     public static void main(String[] args) {
-        String pat = "(.*" + args[0] + ".*)";
+        String pat = "(" + args[0] + ")";
         NFA nfa = new NFA(pat);
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
